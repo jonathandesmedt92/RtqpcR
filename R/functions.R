b272e138650078ab39775e9370ebadb63de8ed67
Jonathan De Smedt <jonathandesmedt92@gmail.com>
2020-04-13 21:43:43 +0200

    Update

--- R/functions.R
+++ R/functions.R
@@ -6,3 +6,37 @@
 
 # Include the class definitions
 #' @include classes.R
+
+#######
+
+#' Read in qPCR data.
+#'
+#' \code{read_qpcr} reads in and formats the RT-qPCR expression data.
+#'
+#' @name read_qpcr
+#' @title read_qpcr
+#' @param qpcr An empty instance of the qpcr class.
+#' @param files A vector of paths pointing to qPCR raw data files.
+#' @param keep_undetected A logical indicating whether to keep undetected CT values and set them to 40, or to simply delete those entries. Defaults to TRUE.
+#' @export
+#' @import dplyr
+#' @import readxl
+#' @return An updated instance of the qpcr class.
+read_qpcr<-function(qpcr,files,keep_undetected=TRUE){
+  # Read in the data
+  data<-lapply(files, readxl::read_excel, sheet="Results", skip=35)
+  # Bind rows
+  data<-lapply(data, FUN=function(x){
+    x[]<-lapply(x,as.character)
+  })
+  data<-data.frame(dplyr::bind_rows(data),stringsAsFactors = F)
+  # Format
+  data<-data[,c("Sample.Name","Target.Name","CT")]
+  data<-data[complete.cases(data),]
+  data$CT[data$CT=="Undetermined"]<-40
+  data$CT<-as.numeric(data$CT)
+  names(data)<-c("Sample","Gene","CT")
+  qpcr@ct<-data
+  return(data)
+}
+
